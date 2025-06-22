1c49c21b7a924021581f55a4834924208ebbc794
Abhishek-B-R <byteblaster1468@gmail.com>
2025-06-22 11:16:34 +0530

    changed leva component's position

--- portfolio/src/sections/Hero.jsx
+++ portfolio/src/sections/Hero.jsx
@@ -56,9 +56,9 @@ export default function Hero() {
             </div>
 
             <div className="w-full h-full absolute inset-0">
+                {/* <Leva/> */}
                 <Canvas className="w-full h-full">
                     <Suspense fallback={<CanvasLoader/>}>
-                        {/* <Leva/> */}
                         <PerspectiveCamera makeDefault position={[0,0,30]}/>
                         <HeroCamera isMobile={isMobile} >
                             <Lucario 
